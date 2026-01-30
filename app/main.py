1de0185cb3ab77c6e8218277ea69cbe7f4edb460
codeguyakash <codeguyakash@gmail.com>
2026-01-30 16:36:13 +0530

    dddddasdasdasd

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ books: List[Book]=[]
 @app.get("/")
 def read_root():
     return {
-        "message": "Server is running final 2",
+        "message": "Server is running",
         "app_version": APP_VERSION,
         "env": ENV, 
         "status": 'success',
